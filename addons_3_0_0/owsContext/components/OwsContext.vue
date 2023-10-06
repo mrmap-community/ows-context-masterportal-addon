79204a79352e6d86eaaca8780f20c7eb9ae02259
Lukas Lohoff <lohoff@terrestris.de>
2023-10-06 09:16:13 +0200

    feat: parse wms layers from context

--- addons_3_0_0/owsContext/components/OwsContext.vue
+++ addons_3_0_0/owsContext/components/OwsContext.vue
@@ -3,6 +3,7 @@ import {mapActions, mapGetters, mapMutations} from "vuex";
 import getters from "../store/gettersOwsContext";
 import mutations from "../store/mutationsOwsContext";
 import LightButton from "../../../../src_3_0_0/shared/modules/buttons/components/LightButton.vue";
+import layerCollection from "../../../../src_3_0_0/core/layers/js/layerCollection";
 
 /**
  * @module modules/OwsContext */
@@ -20,40 +21,60 @@ export default {
         ...mapGetters("Modules/OwsContext", Object.keys(getters))
     },
     watch: {
-        layerObjects: async function (object) {
-            // Example function for layer import
-            // new layerObjects
-            const addLayer = await this.addLayerToLayerConfig({layerConfig: object, parentKey: "Baselayer"});
+        layerObjects: async function (layerList) {
+            // function for layer import
+            const promises = layerList.map(ll => {
+                return this.addLayerToLayerConfig({layerConfig: ll, parentKey: "Baselayer"});
+            });
+            const results = await Promise.allSettled(promises);
 
-            if (addLayer) {
-                console.log("layer has been added");
+            if (results) {
+                console.log("context has been added");
+            } else {
+                console.log("context has not been added.");
             }
-            else {
-                console.log("layer has been added. not.");
-            }
-
         }
     },
     methods: {
         ...mapActions("Modules/LayerTree", ["removeLayer", "replaceByIdInLayerConfig"]),
         ...mapActions(["addLayerToLayerConfig"]),
         ...mapMutations("Modules/OwsContext", Object.keys(mutations)),
-        parseContext: function () {
-            // dummy context...
-            // return layerObject as an example
-            const dummyObject = {
-                id: "ows-no-label",
-                name: "OSM-WMS-no-labels",
-                typ: "WMS",
-                layers: "OSM-WMS-no-labels",
-                url: "https://ows.terrestris.de/osm/service",
-                version: "1.3.0",
-                visibility: true,
-                type: "layer",
-                showInLayerTree: true
-            };
+        parseContext: async function () {
+            // clear layers
+            layerCollection.clear();
 
-            this.setLayerObjects(dummyObject);
+            const url = '/portal/demo/resources/wmc.json';
+            const response = await fetch(url, {});
+            const context = await response.json();
+            console.log('context', context);
+            const layers = context.features;
+            const mpConfigs = layers.map(l => {
+                if (l.offerings[0].code === 'http://www.opengis.net/spec/owc-atom/1.0/req/wms') {
+                    const getMapOperation = l.offerings[0].operations.find(o => o.code === 'GetMap');
+                    const getCapabilitiesOperation = l.offerings[0].operations.find(o => o.code === 'GetCapabilities');
+                    const getMapUrl = getMapOperation?.href && new URL(getMapOperation?.href);
+                    const getCapabilitiesUrl = getCapabilitiesOperation?.href && new URL(getCapabilitiesOperation?.href);
+
+                    const obj = {
+                        id: "ows-no-label",
+                        name: l.properties.title,
+                        typ: "WMS",
+                        layers: getMapUrl?.searchParams.get('LAYERS'),
+                        url: `${getMapUrl?.origin}${getMapUrl?.pathname}`,
+                        version: getMapUrl?.searchParams.get('VERSION'),
+                        visibility: true,
+                        type: "layer",
+                        showInLayerTree: true
+                    };
+                    return obj;
+                } else {
+                    return {};
+                }
+            });
+
+            // add first 10 layers
+            const firstWmsConfigs = [...mpConfigs].slice(0, 10);
+            this.setLayerObjects(firstWmsConfigs);
         }
     }
 };
@@ -66,7 +87,7 @@ export default {
     >
         <LightButton
             :interaction="parseContext"
-            text="Parse OWS"
+            text="Parse OWS Context"
             icon="bi-alipay"
             customclass="w-100 justify-content-start"
         />
