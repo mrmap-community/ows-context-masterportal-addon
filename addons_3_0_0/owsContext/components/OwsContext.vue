fd4cd26c09a215db2b6c997e7e5a9d716c8cd849
Lukas Lohoff <lohoff@terrestris.de>
2023-12-01 16:36:16 +0100

    fix: remove default url

--- addons_3_0_0/owsContext/components/OwsContext.vue
+++ addons_3_0_0/owsContext/components/OwsContext.vue
@@ -18,8 +18,9 @@ export default {
     data () {
         return {
             // owcUrl: "https://www.geoportal.rlp.de/mapbender/php/mod_exportWmc.php?wmcId=2506&outputFormat=json"
-            owcUrl: "/portal/demo/resources/examples/wmc_metadata.json"
             // owcUrl: "/portal/demo/resources/examples/wmc.json"
+            // owcUrl: "/portal/demo/resources/examples/wmc_metadata.json"
+            owcUrl: ""
         };
     },
     computed: {
